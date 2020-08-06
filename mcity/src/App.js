6cbe55193defb5297349496d21a61f8eefd7688c
Aramex86 <slavbez@mail.ru>
2020-08-06 13:26:21 +0300

    Auth and Dashboard component and authitication

--- mcity/src/App.js
+++ mcity/src/App.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import Layout from './HOC/Layout';
 import Home from './componets/Home/Home';
+import SingIn from './componets/SingIn/SingIn';
+import Dashboard from './componets/Admin/Dashboard';
 import {Switch , Route} from 'react-router-dom';
 
 const App=(props)=> {
   return (
    <Layout>
      <Switch>
+     <Route exact component={Dashboard} path='/dashboard'/>
+        <Route exact component={SingIn} path='/sign-in'/>
         <Route exact component={Home} path='/'/>
      </Switch>
   </Layout>
---
+++ mcity/src/componets/SingIn/SingIn.jsx
@@ -0,0 +1,120 @@
+import React, {Component} from 'react';
+import FormData from '../../componets/utiles/FormData';
+import {validation} from '../../componets/common/inputValidation';
+import {firebase} from '../../firebase';
+
+class SingIn extends Component {
+  state = {
+    formError: false,
+    formSucces: '',
+    formdata: {
+      email: {
+        element: 'input',
+        value: '',
+        config: {
+          name: 'email_input',
+          type: 'email',
+          placeholder: 'Enter you email',
+        },
+        validation: {
+          required: true,
+          email: true,
+        },
+        valid: false,
+        validationMessage: '',
+      },
+      password: {
+        element: 'input',
+        value: '',
+        config: {
+          name: 'password_input',
+          type: 'password',
+          placeholder: 'Enter you password',
+        },
+        validation: {
+          required: true,
+        },
+        valid: false,
+        validationMessage: '',
+      },
+    },
+  };
+
+  updateForm = (element) => {
+    const newFormdata = {...this.state.formdata};
+    const newElement = {...newFormdata[element.id]};
+
+    newElement.value = element.event.target.value;
+
+    let validData = validation(newElement);
+    newElement.valid = validData[0];
+    newElement.validationMessage = validData[1];
+
+    newFormdata[element.id] = newElement;
+
+    this.setState({
+      formError: false,
+      formdata: newFormdata,
+    });
+  };
+
+  submitForm = (event) => {
+    event.preventDefault();
+
+    let dataToSubmit = {};
+    let formIsValid = true;
+
+    for (let key in this.state.formdata) {
+      dataToSubmit[key] = this.state.formdata[key].value;
+      formIsValid = this.state.formdata[key].valid && formIsValid;
+    }
+
+    if (formIsValid) {
+      firebase.auth().signInWithEmailAndPassword(
+          dataToSubmit.email,
+          dataToSubmit.password
+      ).then(()=>{
+      this.props.history.push('/dashboard');
+
+      }).catch(error =>{
+          this.setState({
+              formError:true
+          });
+      });
+    } else {
+      this.setState({
+        formError: true,
+      });
+    }
+  };
+
+  render() {
+    return (
+      <div className="container">
+        <div className="signin_wrapper" style={{margin: '100px'}}>
+            <h2>Please Login</h2>
+          <form onSubmit={(event) => this.submitForm(event)}>
+            <FormData
+              id={'email'}
+              formdata={this.state.formdata.email}
+              change={(element) => this.updateForm(element)}
+            />
+            <FormData
+              id={'password'}
+              formdata={this.state.formdata.password}
+              change={(element) => this.updateForm(element)}
+            />
+            {this.state.formError ? (
+                <div className="error_label">Somethisg is wrong, try again</div>
+              ) : null}
+            <button onClick={(event) => this.submitForm(event)}>
+                Sign in
+              </button>
+          </form>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default SingIn;
