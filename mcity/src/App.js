0d86d42f0120a7b48b1bd131494d7cccaff8fd1a
Aramex86 <slavbez@mail.ru>
2020-08-18 16:26:49 +0300

    Theteam component

--- mcity/src/App.js
+++ mcity/src/App.js
@@ -1,75 +1,40 @@
-import React from "react";
-import { Switch } from "react-router-dom";
-import Layout from "./HOC/Layout";
+import React from 'react';
+import { Switch } from 'react-router-dom';
+import Layout from './HOC/Layout';
 
-import Home from "./componets/Home/Home";
-import SingIn from "./componets/SingIn/SingIn";
-import Dashboard from "./componets/Admin/Dashboard";
+import Home from './componets/Home/Home';
+import SingIn from './componets/SingIn/SingIn';
+import Dashboard from './componets/Admin/Dashboard';
 import AdminMatches from './componets/Admin/Matches/Admin_matches';
 import AddEditMatch from './componets/Admin/Matches/AddEditMatch';
 import AdminPlayers from './componets/Admin/Players/AdminPlayers';
 import AddEditPlayers from './componets/Admin/Players/AddEditPlayers';
+import TheTeam from './componets/The-team/TheTeam';
 
-
-
-import PrivateRoute from "./componets/AuthRoutes/PrivateRouts";
+import PrivateRoute from './componets/AuthRoutes/PrivateRouts';
 import PublicRoutes from './componets/AuthRoutes/PublicRoutes';
 
 const App = (props) => {
-  return (
-    <Layout>
-      <Switch>
-      <PrivateRoute
-          {...props}
-          path="/admin_players"
-          exact
-          component={AdminPlayers}
-        />
-      <PrivateRoute
-          {...props}
-          path="/admin_players/add_players"
-          exact
-          component={AddEditPlayers}
-        />
-      <PrivateRoute
-          {...props}
-          path="/admin_players/add_players/:id"
-          exact
-          component={AddEditPlayers}
-        />
-        <PrivateRoute
-          {...props}
-          path="/dashboard"
-          exact
-          component={Dashboard}
-        />
-        <PrivateRoute
-          {...props}
-          path="/admin_matches"
-          exact
-          component={AdminMatches}
-        />
-        
-        <PrivateRoute
-          {...props}
-          path="/admin_matches/edit_match"
-          exact
-          component={AddEditMatch}
-        />
-        <PrivateRoute
-          {...props}
-          path="/admin_matches/edit_match/:id"
-          exact
-          component={AddEditMatch}
-        />
+	return (
+		<Layout>
+			<Switch>
+				<PrivateRoute {...props} path="/admin_players" exact component={AdminPlayers} />
+				<PrivateRoute {...props} path="/admin_players/add_players" exact component={AddEditPlayers} />
+				<PrivateRoute {...props} path="/admin_players/add_players/:id" exact component={AddEditPlayers} />
+				<PrivateRoute {...props} path="/dashboard" exact component={Dashboard} />
+				<PrivateRoute {...props} path="/admin_matches" exact component={AdminMatches} />
 
-        {/* <Route exact component={Dashboard} path='/dashboard'/> */}
-        {/* <Route exact component={SingIn} path="/sign-in" /> */}
-        <PublicRoutes {...props} restricted={true} path="/sign-in" exact component={SingIn}/>
-        <PublicRoutes {...props} restricted={false} path="/" exact component={Home}/>
-        {/* <Route exact component={Home} path="/" /> */}
-      </Switch>
-    </Layout>
-  );
+				<PrivateRoute {...props} path="/admin_matches/edit_match" exact component={AddEditMatch} />
+				<PrivateRoute {...props} path="/admin_matches/edit_match/:id" exact component={AddEditMatch} />
+
+				{/* <Route exact component={Dashboard} path='/dashboard'/> */}
+				{/* <Route exact component={SingIn} path="/sign-in" /> */}
+				<PublicRoutes {...props} restricted={true} path="/sign-in" exact component={SingIn} />
+				<PublicRoutes {...props} restricted={false} path="/" exact component={Home} />
+				<PublicRoutes {...props} restricted={false} path="/the_team" exact component={TheTeam} />
+				{/* <Route exact component={Home} path="/" /> */}
+			</Switch>
+		</Layout>
+	);
 };
 export default App;
---
+++ mcity/src/componets/The-team/TheTeam.jsx
@@ -0,0 +1,99 @@
+import React, { Component, useState, useEffect } from 'react';
+import PlayerCard from '../common/PlayerCard';
+import Fade from 'react-reveal/Fade';
+
+import Stripes from '../../images/stripes.png';
+import { dataPlayers, firebase, dataBase } from '../../firebase';
+import { firebaseLooper } from '../common/ConvertFunction';
+import { Promise } from 'core-js';
+
+const Theteam = (props) => {
+	const [loading, setLoading] = useState(true);
+	const [players, setPlayers] = useState([]);
+
+	useEffect(() => {
+		getPlayers();
+	}, []);
+
+	const getPlayers = () => {
+		dataPlayers.once('value').then((snapshot) => {
+			const players = firebaseLooper(snapshot);
+			let promises = [];
+
+			for (let key in players) {
+				promises.push(
+					new Promise((resolve, reject) => {
+						firebase
+							.storage()
+							.ref('players')
+							.child(players[key].image)
+							.getDownloadURL()
+							.then((url) => {
+								players[key].url = url;
+								resolve();
+							});
+					})
+				);
+			}
+
+			Promise.all(promises).then(() => {
+                setLoading(false);
+                setPlayers(players);
+                
+			});
+		});
+    };
+    
+   const showplayersByCategory = (category) => (
+        players?players.map((player,i)=>{
+                return player.position === category ?
+                    <Fade left delay={i*20} key={i}>
+                        <div className="item">
+                            <PlayerCard
+                                number={player.number}
+                                name={player.name}
+                                lastname={player.lastname}
+                                bck={player.url}
+                            />
+                        </div>
+                    </Fade>
+                :null
+            })
+        :null
+    )
+
+	return (
+		<div
+			className="the_team_container"
+			style={{
+				background: `url(${Stripes}) repeat`,
+			}}
+		>
+			{!loading ? (
+				<div>
+					<div className="team_category_wrapper">
+						<div className="title">Keepers</div>
+						<div className="team_cards">{showplayersByCategory('Keeper')}</div>
+					</div>
+
+					<div className="team_category_wrapper">
+						<div className="title">Defence</div>
+						<div className="team_cards">{showplayersByCategory('Defence')}</div>
+					</div>
+
+					<div className="team_category_wrapper">
+						<div className="title">Midfield</div>
+						<div className="team_cards">{showplayersByCategory('Midfield')}</div>
+					</div>
+
+					<div className="team_category_wrapper">
+						<div className="title">Strikers</div>
+						<div className="team_cards">{showplayersByCategory('Striker')}</div>
+					</div>
+				</div>
+			) : null}
+		</div>
+	);
+};
+
+export default Theteam;
